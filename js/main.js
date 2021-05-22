4022c6b6a278aefd7c61b465953ec0076e3cb0a1
jasmine_r <joycesdaughter82@gmail.com>
2021-05-22 16:47:25 -0500

    Updated javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// hamburger menu
+function openNav() {
+    document.getElementById("mySidebar").style.width = "375px";
+    document.getElementById("main").style.marginLeft = "0";
+    }
+  
+   function closeNav() {
+   document.getElementById("mySidebar").style.width = "0";
+   document.getElementById("main").style.marginLeft= "0";
+    }
